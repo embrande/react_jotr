8c7419869d6d0d88da26d3e7907cb9ba1e02442f
U-ADS\embrande <embrande@IN-STSV-25JNJB2.ads.iu.edu>
2017-10-30 12:28:50 -0400

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  context: __dirname + "/app",
+
+  entry: "./js/app.js",
+
+  output: {
+    filename: "app.js",
+    path: __dirname + "/dist",
+  }, 
+
+  resolve: {
+    extensions: ['', '.js', '.jsx', '.json']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loaders: ["babel-loader"]
+      }
+    ]
+  }
+
+  
+};
